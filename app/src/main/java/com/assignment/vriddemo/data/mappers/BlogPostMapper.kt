7eb79685485d6f4a4dc25e5287480bba999bde6e
sujalkumar <www.sujalmuz1245@gmail.com>
2025-05-10 10:51:27 +0530

    Shortened the estimated time for conciseness.

--- app/src/main/java/com/assignment/vriddemo/data/mappers/BlogPostMapper.kt
+++ app/src/main/java/com/assignment/vriddemo/data/mappers/BlogPostMapper.kt
@@ -49,7 +49,10 @@ fun extractReadTime(content: String): String {
 
     return if (startIndex != -1) {
         val subString = content.substring(startIndex + startKeyword.length)
-        val endIndex = subString.indexOf("seconds") + "seconds".length
+            .replace("minutes", "mins")
+            .replace("seconds", "secs")
+
+        val endIndex = subString.indexOf("secs") + "secs".length
         if (endIndex != -1) {
             subString.substring(0, endIndex).trim()
         } else {
